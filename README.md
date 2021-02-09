f4b67177e7f2fbf63fb41b6e5859740b24203762
gdarquie <gaetan.darquie@gmail.com>
2021-02-09 17:50:06 +0100

    wip: add first logic for conditional text interpretaion

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Texty, a tool for writing dynamically texts in fictions
 
-For french
+## Gender conversion
+
+In french
 
 ```
 <:'elle','il'> est né<:'e',> à Lyon.
@@ -11,8 +13,16 @@ Will become, if gender narrator is female associated:
 elle est née à Lyon
 ```
 
+## Dynamic text
+
 We can inject dynamic value inside text with the following syntax:
 
 ```
 <:name> is a person.
+```
+
+## Conditional text (wip)
+
+```
+<:if(sanity > 4)"You read the article", "You try to read but you don't understand anything.">
 ```
--- index.test.js
+++ index.test.js
@@ -11,3 +11,15 @@ test('convert gender when M', () => {
     const output  = "<p>Toute votre vie, vous vous êtes occupé des autres, infirmier depuis vos 18 ans, vous avez toujours exercé ce métier sans interruption.</p>";
     expect(texty.convert(input, 'M')).toBe(output);
 })
+
+test('convert dynamic text', () => {
+    const input = "<:name> is a person.";
+    const output = "Okita is a person.";
+    expect(texty.convert(input, 'F', {"name": "Okita"})).toBe(output);
+})
+
+test('convert conditional text', () => {
+    const input = '<:if(sanity > 4)"You read the article", "You try to read but you don\'t understand anything">!!!';
+    const output = 'You read the article';
+    expect(texty.convert(input, 'M', {"sanity": 6})).toBe(output);
+})
